76efaa1e97355921a8ee367c1ce9f21bb599721d
tolluset <dlqud19@gmail.com>
2023-11-01 01:49:36 +0900

    chore: add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E5DFA9",
+        secondary: "#9381FF",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
