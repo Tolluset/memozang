b383d1557061255f9e9acb54d630c54af0288821
tolluset <dlqud19@gmail.com>
2023-11-01 02:31:38 +0900

    fix: add logic in logout button

--- src/ds/Button.tsx
+++ src/ds/Button.tsx
@@ -1,17 +1,21 @@
+import React from "react";
+
 type Variant = "primary" | "secondary";
 
 export default function Button({
   variant = "primary",
   children,
+  ...rest
 }: {
   variant?: Variant;
   children: React.ReactNode;
-}) {
+} & React.ComponentPropsWithoutRef<"button">) {
   const bgColor = variant === "primary" ? "bg-primary" : "bg-secondary";
 
   return (
     <button
       className={`${bgColor} w-fit font-bold text-[1.8rem] p-3 rounded-2xl`}
+      {...rest}
     >
       {children}
     </button>
--- src/features/auth/LogoutButton.tsx
+++ src/features/auth/LogoutButton.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import { Database } from "~/models/database.types";
 import Button from "~/ds/Button";
 
 export default function LogoutButton() {
-  return <Button variant="secondary">Logout</Button>;
+  const supabase = createClientComponentClient<Database>();
+
+  const onClickLogoutButton = async () => {
+    await supabase.auth.signOut();
+    window.location.reload();
+  };
+
+  return (
+    <Button variant="secondary" onClick={onClickLogoutButton}>
+      Logout
+    </Button>
+  );
 }
